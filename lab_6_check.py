9f8cdad2b43639fe0cc599db189c6280cbd477fa
Marco <marcorene.estrada@me.com>
2020-07-13 14:35:20 -0400

    lab 6

---
+++ lab_6_check.py
@@ -0,0 +1,14 @@
+print(42)
+print(42)
+print("192.168.30.5", "A1:02:CC:04", "255.255.255.0")
+a=3.14
+print("a = ", a)
+print("a = ", a)
+print("a = \n", a)
+print("a", "b")
+print("a","b", sep="")
+print(192,168,178,42,sep=".")
+print("a","b", sep=":)")
+print("I am thinking", end="... ... ... \n")
+print("I am thinking", end="!!!")
+

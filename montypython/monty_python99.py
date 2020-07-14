e8dd55a271470779edff1ef69ad3cf18e9569fa0
Marco <marcorene.estrada@me.com>
2020-07-14 17:02:21 -0400

    init commit

--- montypython/monty_python99.py
+++ montypython/monty_python99.py
@@ -5,8 +5,12 @@ answer = " "
 while round < 3 and answer != "Brian":
     round += 1     # increase the round counter by 1
     answer = input('Finish the movie title, "Monty Python\'s The Life of ______": ')
-    if answer == "Brian": # logic to check if user gave correct answer
+    if answer == "Brian" or answer == "brian" or answer == "BRIAN": # logic to check if user gave correct answer
         print("Correct!")
+        break
+    elif answer == "shrubbery":
+        print("You gave the super secret answer!!")
+        break
     elif round == 3:    # logic to ensure round has not yet reached 3
         print("Sorry, the answer was Brian.")
     else:                 # if answer was wrong

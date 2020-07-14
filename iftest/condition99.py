24e139cd3a181c1eea108b9fdcc4134200c25243
Marco <marcorene.estrada@me.com>
2020-07-14 15:34:18 -0400

    init commit

---
+++ iftest/condition99.py
@@ -0,0 +1,19 @@
+import random
+
+x = random.randint(0, 10)
+
+print(x)
+
+if x > 5:
+    print(x, " es mayor que 5")
+#    while x > 5:
+ #       print("empezando a restar")
+  #      x = x-1
+   #     print(x)
+    #pass
+elif x ==5:
+    print(x, "es 5")
+else:
+    print(x, " es menor que 5")
+
+    

23b31d0064368f948bf3a1d24b4126db04ddc597
Marco <marcorene.estrada@me.com>
2020-07-14 14:31:26 -0400

    init commit

--- mytest.py
+++ mytest.py
@@ -1,19 +1,10 @@
 #!/usr/bin/env python3
-"""Alta3 Research || Author: RZFeeser@alta3.com"""
+import datetime
+import random
+import os
+import ztest
 
-def main():
-    """ Run-time code"""
-    # create a small string
-    lilstring = "Alta3 Research offers classes on Python coding"
-    newlist = lilstring.split(" ") # this returns a list
-    print(newlist)
-
-    # create a list of strings
-    myiplist = ["192", "168", "0", "12"]
-    # set singleip as the result of joining the list myiplist around the "."
-    singleip = ".".join(myiplist)
-    # display singleip
-    print(singleip)
-
-# call our main function
-main()
+print(datetime.datetime.now())
+print(random.randint(0,10))
+#os.system("ls")
+ztest.main()

4235cb97878f5aaee06f7eba94023a9882b60397
Marco <marcorene.estrada@me.com>
2020-07-15 10:13:02 -0400

    init commit

---
+++ introfor/challenge_loop.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# Create a list of your favorite foods (at least 5 items)
+# Iterate through that list with a for loop
+# For each item in your list, print "I can't wait to eat some _____!"
+food = ["chicken", "pizza", "steak", "sushi", "hotdog"]
+
+for x in food:
+    print ("I can't wait to eat some", x, "!")
+#print("\nOur loop has ended.") # print when loop has finished

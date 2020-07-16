f3b88f4d6a4f5af88d94d18786dbecb63961f2c9
Marco <marcorene.estrada@me.com>
2020-07-16 11:01:09 -0400

    another line

--- netfacd/interface_reader.py
+++ netfacd/interface_reader.py
@@ -14,3 +14,35 @@ for i in netifaces.interfaces():
     except:          # This is a new line
         print('Could not collect adapter information') # Print an error message
 
+def ip_function(ip="lo"):
+
+    for i in netifaces.interfaces():
+        print('\n****** details of interface - IP ' + ip + ' ******')
+        try:
+            print((netifaces.ifaddresses(ip)[netifaces.AF_INET])[0]['addr']) # Prints the IP address
+        except:          # This is a new line
+            print('Could not collect adapter information') # Print an error messag
+            
+def mac_function(mc="lo"):
+
+    for i in netifaces.interfaces():
+        print('\n****** details of interface - MAC ' + mc + ' ******')
+        try:
+            print((netifaces.ifaddresses(mc)[netifaces.AF_LINK])[0]['addr']) # Prints the MAC address 
+        except:          # This is a new line
+            print('Could not collect adapter information') # Print an error messag
+
+def final(f="lo"):
+
+    print('\n****** details of interface - ' + f + ' ******')
+    try:
+        print('MAC: ', end='') # This print statement will always print MAC without an end of line
+        print((netifaces.ifaddresses(f)[netifaces.AF_LINK])[0]['addr']) # Prints the MAC address
+        print('IP: ', end='')  # This print statement will always print IP without an end of line
+        print((netifaces.ifaddresses(f)[netifaces.AF_INET])[0]['addr']) # Prints the IP address
+    except:          # This is a new line
+        print('Could not collect adapter information') # Print an error message
+
+ip_function()
+mac_function("ens3")
+final("ens3")
